449ae8ec8cf383af36c3cec9caa6f41070b298b3
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-02-14 20:43:56 -0600

    updates HackReactor

--- prep/week2/events.js
+++ prep/week2/events.js
@@ -5,3 +5,47 @@
  * Time: 4:00 PM
  * Content:
  */
+/*
+//generates a random number between 0 and 1 and adds it to #
+var random = '#' + Math.random();
+console.log(random);
+//same as above but this time it converts it into a string before adding it
+random = '#' + Math.random().toString();
+console.log(random);
+//same as above but uses a radix of 16 which converts number into hexadecimal
+random = '#' + Math.random().toString(16);
+console.log(random);
+//same as above but it also slices the newly converted string beginning at the character after the decimal
+random = '#' + Math.random().toString(16).slice(2, 8);
+console.log(random);*/
+
+
+var recipe = ['add sugar', 'add flower', 'add water'];
+var bucket_list = ['be happy', 'eat well', 'be a good father'];
+
+var html = "<ul>";
+var i = 0;
+
+while( i < recipe.length){
+  html += "<li>" + recipe[i] + "</li>";
+  i++;
+}
+
+html += "</ul>";
+
+$('#recipe').append(html);
+
+
+
+///////////////////////////////////////////////
+html = "<ol>";
+i = 0;
+
+while( i < bucket_list.length){
+  html += "<li>" + bucket_list[i] + "</li>";
+  i++;
+}
+
+html += "</ol>";
+
+$('#bucket_list').append(html);
---
+++ prep/week2/objects.js
@@ -0,0 +1,25 @@
+/**
+ * Project: HackReactor.
+ * By: Ronny Rosabal
+ * Date: 2/10/2016
+ * Time: 8:50 PM
+ * Content:
+ */
+
+var dream_car = {};
+
+dream_car.make = "BMW";
+dream_car.model = "700 series";
+dream_car.color = "space gray";
+dream_car.year = 2016;
+dream_car.registered = true;
+
+var html = "<ul>";
+
+for(var prop in dream_car){
+  html += "<li>" + prop + " : " + dream_car[prop] + "</li>";
+}
+
+html += "</ul>";
+
+$('#result').append(html);
