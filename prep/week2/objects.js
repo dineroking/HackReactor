3e6d08574cbf605aef2f8aa7adf3f23fe847af0d
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-02-26 13:42:41 -0600

    updates

--- prep/week2/objects.js
+++ prep/week2/objects.js
@@ -1,28 +1,11 @@
 /**
  * Project: HackReactor.
  * By: Ronny Rosabal
- * Date: 2/10/2016
- * Time: 8:50 PM
+ * Date: 2/11/16
+ * Time: 6:00 PM
  * Content:
  */
 
-var dream_car = {};
-
-dream_car.make = "BMW";
-dream_car.model = "700 series";
-dream_car.color = "space gray";
-dream_car.year = 2016;
-dream_car.registered = true;
-
-var html = "<ul>";
-
-for(var prop in dream_car){
-  html += "<li>" + prop + " : " + dream_car[prop] + "</li>";
-}
-
-html += "</ul>";
-
-$('#result').append(html);
 var name = "El FEO";
 
 alert(name);
--- prep/week3/finalProject/js/project.js
+++ prep/week3/finalProject/js/project.js
@@ -5,3 +5,12 @@
  * Time: 6:45 PM
  * Content:
  */
+
+
+var windowHeight = $( document ).height();
+var windowWidth = $( document).width();
+var wrapperHeight = windowHeight * .75;
+$('#wrapper').css('height', wrapperHeight + 'px');
+
+console.log(windowHeight);
+console.log(wrapperHeight);
